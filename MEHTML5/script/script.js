42d8088abadd788edad784d80a49b1ceae8ab7ad
vigonz06 <victorglez96@gmail.com>
2018-11-02 23:20:38 +0100

    cada vez mas cosas

--- MEHTML5/script/script.js
+++ MEHTML5/script/script.js
@@ -1,12 +1,13 @@
-var downs = ["SCD", "GCD"];
-var ups = ["SCU", "GCU"];
-var id = ["SDD", "GDD"];
+var id =    ["SDD", "GDD", "SDDC0", "SDDC1", "SDDC2", "SDDC3", "SDDC4", "GDDC0", "GDDC1", "GDDC2", "GDDC3"];
+var ups =   ["SCU", "GCU", "SCU0", "SCU1", "SCU2", "SCU3", "SCU4", "GCU0", "GCU1", "GCU2", "GCU3"];
+var downs = ["SCD", "GCD", "SCD0", "SCD1", "SCD2", "SCD3", "SCD4", "GCD0", "GCD1", "GCD2", "GCD3"];
 
 function Display(i)
 {
 	var j, k;
 	
-	k = (i == 0) ? 1 : 0;
+	j = (i > 1) ? (i > 6) ?  7 : 2 : 0;
+	k = (i > 1) ? (i > 6) ? 10 : 6 : 1;
 	
 	document.getElementById(id[i]).classList.toggle('show');
 	
@@ -21,12 +22,21 @@ function Display(i)
 		document.getElementById(downs[i]).style.display = 'none';
 	}
 	
-	if(document.getElementById(id[k]).classList.contains('show'))
+	for(; j <= k; j++)
 	{
-		document.getElementById(id[k]).classList.remove('show');
-		
-		document.getElementById(downs[k]).style.display = 'inline-block';
-		document.getElementById(ups[k]).style.display = 'none';
+		if(!(j == i))
+		{
+			if(document.getElementById(id[j]).classList.contains('show'))
+			{
+				document.getElementById(id[j]).classList.remove('show');
+			}
+			
+			if(document.getElementById(downs[j]).style.display === 'none')
+			{
+				document.getElementById(downs[j]).style.display = 'inline-block';
+				document.getElementById(ups[j]).style.display = 'none';
+			}
+		}
 	}
 }
 
@@ -34,7 +44,7 @@ window.onclick = function(event)
 {
 	var i, j, k;
 	
-	if(!event.target.matches('.SNButton'))
+	if(!event.target.matches('.SNButton') && !event.target.matches('.DropButton'))
 	{
 		for(i in downs)
 		{
