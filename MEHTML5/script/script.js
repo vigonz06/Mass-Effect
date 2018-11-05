c99a63449004acdc34502a228c0deeb62b69eede
vigonz06 <victorglez96@gmail.com>
2018-11-05 23:13:09 +0100

    skills to be finished

--- MEHTML5/script/script.js
+++ MEHTML5/script/script.js
@@ -1,8 +1,9 @@
 var Ids = [
-[{ButtonId:"GDD", CaretUpId:"GCU", CaretDownId:"GCD"}, {ButtonId:"SDD", CaretUpId:"SCU", CaretDownId:"SCD"}, {ButtonId:"CDD", CaretUpId:"CCU", CaretDownId:"CCD"}],
+[{ButtonId:"GDD", CaretUpId:"GCU", CaretDownId:"GCD"}, {ButtonId:"SDD", CaretUpId:"SCU", CaretDownId:"SCD"}, {ButtonId:"CDD", CaretUpId:"CCU", CaretDownId:"CCD"}, {ButtonId:"PDD", CaretUpId:"PCU", CaretDownId:"PCD"}],
 [{ButtonId:"GDDC0", CaretUpId:"GCU0", CaretDownId:"GCD0"}, {ButtonId:"GDDC1", CaretUpId:"GCU1", CaretDownId:"GCD1"}, {ButtonId:"GDDC2", CaretUpId:"GCU2", CaretDownId:"GCD2"}, {ButtonId:"GDDC3", CaretUpId:"GCU3", CaretDownId:"GCD3"}],
 [{ButtonId:"SDDC0", CaretUpId:"SCU0", CaretDownId:"SCD0"}, {ButtonId:"SDDC1", CaretUpId:"SCU1", CaretDownId:"SCD1"}, {ButtonId:"SDDC2", CaretUpId:"SCU2", CaretDownId:"SCD2"}, {ButtonId:"SDDC3", CaretUpId:"SCU3", CaretDownId:"SCD3"}, {ButtonId:"SDDC4", CaretUpId:"SCU4", CaretDownId:"SCD4"}],
-[{ButtonId:"CDDC0", CaretUpId:"CCU0", CaretDownId:"CCD0"}, {ButtonId:"CDDC1", CaretUpId:"CCU1", CaretDownId:"CCD1"}, {ButtonId:"CDDC2", CaretUpId:"CCU2", CaretDownId:"CCD2"}]
+[{ButtonId:"CDDC0", CaretUpId:"CCU0", CaretDownId:"CCD0"}, {ButtonId:"CDDC1", CaretUpId:"CCU1", CaretDownId:"CCD1"}, {ButtonId:"CDDC2", CaretUpId:"CCU2", CaretDownId:"CCD2"}],
+[{ButtonId:"PDDC0", CaretUpId:"PCU0", CaretDownId:"PCD0"}, {ButtonId:"PDDC1", CaretUpId:"PCU1", CaretDownId:"PCD1"}, {ButtonId:"PDDC2", CaretUpId:"PCU2", CaretDownId:"PCD2"}]
 ];
 
 function Display(i, j)
